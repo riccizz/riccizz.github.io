084c7f01d48e8909fdbc207cedf2fc17847935e5
Ricci Zhang <riccizz0045@gmail.com>
2025-02-11 05:52:05 -0600

    adapt to phone

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -6,13 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
         let bibtexContent = card.querySelector(".bibtex-content");
 
         abstractButton.addEventListener("click", function () {
-            // 关闭 Bibtex，切换 Abstract
             bibtexContent.classList.add("hidden");
             abstractContent.classList.toggle("hidden");
         });
 
         bibtexButton.addEventListener("click", function () {
-            // 关闭 Abstract，切换 Bibtex
             abstractContent.classList.add("hidden");
             bibtexContent.classList.toggle("hidden");
         });
